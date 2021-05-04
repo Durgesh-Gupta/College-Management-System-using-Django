5f13fb80c218c7f54e812d744fb265e685da6a59
durgesh gupta <durgesh10gupta@gmail.com>
2021-05-04 17:26:34 +0530

    ready for deploy

--- CollegeManagementProject/CollegeManagementProject/settings.py
+++ CollegeManagementProject/CollegeManagementProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nz@+snunuf6vgsa3g^c$#0$f_l5)menv-*ntz(k59m8jmhw#!p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1","s-m-system.herokuapp.com/"]
 
 
 # Application definition
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
