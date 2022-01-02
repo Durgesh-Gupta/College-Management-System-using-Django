b972d11f1e0d4a43bf3224addb78e94c0d23dc09
dgupta <DurgeshG@123>
2022-01-02 14:42:50 +0530

    fix:move on old

--- CollegeManagementProject/CollegeManagementProject/urls.py
+++ CollegeManagementProject/CollegeManagementProject/urls.py
@@ -18,7 +18,12 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('CollegeApp.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
